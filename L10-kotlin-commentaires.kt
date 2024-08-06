6e6e2ac4deb796173bf66ded650078acc4dc9966
Ramzy Kermad <kermad.r@sncf.fr>
2024-08-06 14:51:46 +0200

    Lesson 10 init

---
+++ L10-kotlin-commentaires.kt
@@ -0,0 +1,9 @@
+/**
+ * Convertit une température de Celsius à Fahrenheit.
+ *
+ * @param celsius La température en degrés Celsius.
+ * @return La température convertie en degrés Fahrenheit.
+ */
+fun celsiusToFahrenheit(celsius: Double): Double {
+    return celsius * 9 / 5 + 32
+}
