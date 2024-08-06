ecc1e21b3494c6cbf9c75451082409cfee982a9e
Ramzy Kermad <kermad.r@sncf.fr>
2024-08-06 12:39:41 +0200

    Lesson 4 init

---
+++ L4-kotlin-conditions.kt
@@ -0,0 +1,7 @@
+val temperature = 25
+
+if (temperature > 30) {
+    println("It's a hot day.") // Code exécuté si temperature est supérieure à 30
+} else {
+    println("It's a pleasant day.") // Code exécuté sinon
+}
